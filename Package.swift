cf284510542806505ea562fa527bcba46a24cde1
noel_chiang <noel_chiang@friday.tw>
2025-07-10 14:55:44 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "H2VStack",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "H2VStack",
+            targets: ["H2VStack"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "H2VStack"),
+        .testTarget(
+            name: "H2VStackTests",
+            dependencies: ["H2VStack"]
+        ),
+    ]
+)

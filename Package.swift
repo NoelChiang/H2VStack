6af2c6efa63772df8c23052f2e9a92f13491d950
noel_chiang <noel_chiang@friday.tw>
2025-07-11 09:33:16 +0800

    correct syntax

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "H2VStack",
     platforms: [
         .iOS(.v16)
-    ]
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
